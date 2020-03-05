c49dccb1c72c3c50c69cb7c244ce8720de546eb3
Gonzalo Campos <erick.campos@wizeline.com>
2020-03-04 22:01:59 -0600

    Creating examples file in kotlin

---
+++ src/test/kotlin/com/gonzz/rx/java/study/chaptertwo/ChapterTwoExamplesKotlin.kt
@@ -0,0 +1,11 @@
+package com.gonzz.rx.java.study.chaptertwo
+
+import io.reactivex.Observable
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+class ChapterTwoExamplesKotlin {
+
+}
