6edc4324490f3c69911562bf59b5f3b9bbd09a03
Gonzalo Campos <erick.campos@wizeline.com>
2020-03-15 22:39:17 -0600

    zip() factory in java (no BiFunction was needed)

--- src/test/java/com/gonzz/rx/java/study/chaptertwo/ChapterTwoExamples.java
+++ src/test/java/com/gonzz/rx/java/study/chaptertwo/ChapterTwoExamples.java
@@ -394,5 +394,17 @@ public class ChapterTwoExamples {
         }
     }
 
+    @Test
+    public void zipFactory() {
+
+        Observable<String> source1 =
+                Observable.just("Alpha", "Beta", "Gamma", "Delta",
+                        "Epsilon");
+        Observable<Integer> source2 = Observable.range(1,6);
+        Observable.zip(source1, source2, (s,i) -> s + "-" + i)
+                .subscribe(System.out::println);
+    }
+
+
     private void runProcess() { }
 }
